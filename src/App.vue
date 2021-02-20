61f2d5201e22c234b25c563855af0d7b05b6c8d3
Gretten <shhworkmail@yandex.ru>
2021-02-20 18:34:17 +0300

    style fix

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <v-app>
     <v-main>
-      <Table/>
-    </v-main>
+      <v-container fluid>
+        <Table />
+      </v-container>
+
+    </v-main>v
   </v-app>
 </template>
 
 <script>
-import Table from './components/Table';
+  import Table from './components/Table';
 
-export default {
-  name: 'App',
+  export default {
+    name: 'App',
 
-  components: {
-    Table,
-  },
+    components: {
+      Table,
+    },
 
-  data: () => ({
-    
-  }),
-};
+    data: () => ({
+
+    }),
+  };
 </script>
-
